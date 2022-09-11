2588f0c0b0016b09cb849b2035f911656adbe482
axrno <arnoislam04@gmail.com>
2022-09-10 18:07:04 -0700

    able to add cities to view weather

--- weather_App/views.py
+++ weather_App/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+import requests
+from .models import City
+from .forms import CityForm
 
-# Create your views here.
+def index(request):
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&APPID=9586614fd6568eaa22167ae018676d1c'
+    
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    weather_data = []
+
+    for city in cities:
+
+        r = requests.get(url.format(city)).json()
+        
+        city_weather = {
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
+        }
+
+        weather_data.append(city_weather)
+
+
+    context = {'weather_data': weather_data, 'form': form}
+
+    return render(request, 'weather_App/weather_App.html', context)
